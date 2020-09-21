ba2ac4c51466f4ed284400afe0f449e1819c64f1
jeffmayle <jeffmayle75@gmail.com>
2020-09-21 11:09:55 +0100

    Dropbox code

--- append_to_anacrontab.sh
+++ append_to_anacrontab.sh
@@ -7,7 +7,3 @@
 grep -qxF '1	20	jeff.birthdays	/home/jeff/Dropbox/Linux-stuff/Bash-scripts/Useful/Birthdays/bday.sh' /etc/anacrontab || echo '1	20	jeff.birthdays	/home/jeff/Dropbox/Linux-stuff/Bash-scripts/Useful/Birthdays/bday.sh' >> /etc/anacrontab
 
 grep -qxF '1	23	jeff.birthdays5	/home/jeff/Dropbox/Linux-stuff/Bash-scripts/Useful/Birthdays/bday5.sh' /etc/anacrontab || echo '1	23	jeff.birthdays5 /home/jeff/Dropbox/Linux-stuff/Bash-scripts/Useful/Birthdays/bday5.sh' >> /etc/anacrontab
-
-sudo ln -s '/home/jeff/Dropbox/Languages/Learn-Czech/Do you want to speak Czech?' '/home/jeff/Desktop/Do you want to speak Czech?'
-
-sudo ln -s '/home/jeff/Dropbox/Linux-stuff/Linux-Documentation2.odt' '/home/jeff/Desktop/Linux-Documentation2.odt'
--- installing_dropbox_20.04.sh
+++ installing_dropbox_20.04.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##
 ##
-## This script 
+## This script installs dropbox then creates two soft links to the desktop
 
 sudo bash -c `echo "deb [arch=i386,amd64] http://linux.dropbox.com/ubuntu bionic main" > /etc/apt/sources.list.d/dropbox.list`
 
@@ -11,7 +11,12 @@ sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 1C61A2656FB57B7E4DE0F4C1FC9
 
 sudo apt update
 sudo apt install python3-gpg dropbox
-sudo
+
 
 dropbox start -i
 
+sleep 6000
+
+sudo ln -s '/home/jeff/Dropbox/Languages/Learn-Czech/Do you want to speak Czech?' '/home/jeff/Desktop/Do you want to speak Czech?'
+
+sudo ln -s '/home/jeff/Dropbox/Linux-stuff/Linux-Documentation2.odt' '/home/jeff/Desktop/Linux-Documentation2.odt'
