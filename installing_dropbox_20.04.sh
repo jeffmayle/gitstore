448306374b3f64f2eab49af1827b6c7a0a5f2829
Jeff Mayle <jeff@ubuntu-box>
2023-06-17 11:56:20 +0100

    Intial commit

--- installing_dropbox_20.04.sh
+++ installing_dropbox_20.04.sh
@@ -11,12 +11,4 @@ sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 1C61A2656FB57B7E4DE0F4C1FC9
 
 sudo apt update
 sudo apt install python3-gpg dropbox
-
-
 dropbox start -i
-
-sleep 6000
-
-sudo ln -s '/home/jeff/Dropbox/Languages/Learn-Czech/Do you want to speak Czech?' '/home/jeff/Desktop/Do you want to speak Czech?'
-
-sudo ln -s '/home/jeff/Dropbox/Linux-stuff/Linux-Documentation2.odt' '/home/jeff/Desktop/Linux-Documentation2.odt'
--- post_install-Progress_20.04.sh
+++ post_install-Progress_20.04.sh
@@ -9,9 +9,9 @@ sudo apt install hunspell-en-gb xpad git git-extras tlp tlp-rdw htop vim cheese 
 sudo tlp start 
 
 ##Change screen appearance and move Unity toolbar
-gsettings set org.gnome.desktop.session idle-delay 3600
-gsettings set org.gnome.desktop.screensaver lock-delay 0
-gsettings set com.canonical.Unity.Launcher launcher-position Bottom
+gsettings set org.gnome.desktop.session idle-delay 900
+gsettings set org.gnome.desktop.screensaver lock-delay 3600
+#gsettings set com.canonical.Unity.Launcher launcher-position Bottom # this no longer works in 20.04
 
 ##Adding Skype 
 snap install --classic skype
