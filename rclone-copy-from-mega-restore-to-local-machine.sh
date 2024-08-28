090cc1cc10f8b8b423c737c8b3f3e51fbacef12c
Jeff Mayle <jeff@Ubuntu-20.04.06>
2024-08-28 11:26:52 +0100

    Slight change to rclone script

--- rclone-copy-from-mega-restore-to-local-machine.sh
+++ rclone-copy-from-mega-restore-to-local-machine.sh
@@ -8,4 +8,12 @@
 
 ##rclone copy /home/jeff/Music pCloud:My\ Music/
 
+sudo apt install rclone
+
+# Now connect to Mega
+echo "Connect Mega with rclone"
+echo .
+echo "Press [ENTER] to continue"
+read -s < /dev/tty
+
 rclone copy Mega:Music_Backup /home/jeff/Music
